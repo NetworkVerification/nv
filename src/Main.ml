a7fb092e76026d9795b1d7704244986c26b758fb
Ryan Beckett <rbeckett@princeton.edu>
2018-08-17 15:12:06 -0600

    remove printing

--- src/Main.ml
+++ src/Main.ml
@@ -88,7 +88,6 @@ let run_simulator cfg info decls =
 let main =
   let cfg, rest = argparse default "nv" Sys.argv in
   Cmdline.set_cfg cfg ;
-  Printf.printf "%b\b" cfg.no_caching;
   if cfg.debug then Printexc.record_backtrace true ;
   let file = rest.(0) in
   let ds, info = Input.parse file in
