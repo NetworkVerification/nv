f82de63a3311fc0a1a5a52fde29215e56be26f99
Devon Loehr <devon.loehr@gmail.com>
2018-09-21 10:49:10 -0400

    Update lexer to accept bitwidths on tokens +, int, and numbers]. Parser is not yet updated to comply.

--- src/Integer.ml
+++ src/Integer.ml
@@ -1,5 +1,20 @@
 type t = {size: Z.t; value: Z.t}
 
+let of_string (s : string) : t =
+  let lst = List.map Z.of_string @@ Str.split (Str.regexp "u") s in
+  match lst with
+  | [] -> failwith "This is literally impossible"
+  | value::[] -> {size = Z.of_int 32; value}
+  | value::size::[] -> {size; value}
+  | _ -> failwith "Integer.of_string: Too many values"
+;;
+
+let of_int (n : int) : t =
+  {size = Z.of_int 32; value = Z.of_int n}
+
+let of_size_and_val (value : int) (size : int) : t =
+  {size = Z.of_int size; value = Z.of_int value}
+
 let check x y =
   if not (Z.equal x.size y.size) then
     failwith "integer bit sizes did not match"
@@ -26,3 +41,7 @@ let create sz str =
 let lt x y = check x y ; Z.lt x.value y.value
 
 let leq x y = check x y ; Z.leq x.value y.value
+
+let gt x y = check x y ; Z.gt x.value y.value
+
+let geq x y = check x y ; Z.geq x.value y.value
