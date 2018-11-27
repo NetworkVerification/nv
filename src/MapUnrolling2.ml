9d64f3884dd9ccf4a0cb5d29f8d171bc6b2ec07e
Devon Loehr <dloehr@princeton.edu>
2018-11-26 21:14:47 -0500

    Finish implementing map unrolling for a single type. Not yet tested.

--- src/MapUnrolling2.ml
+++ src/MapUnrolling2.ml
@@ -18,13 +18,13 @@ let get_index keys k =
   | _ -> None
 ;;
 
-let rec unroll_in_exp
+let rec unroll_exp
     (ty : ty)
     (keys : exp list)
     (e : exp)
   : exp
   =
-  let unroll_in_exp e = unroll_in_exp ty keys e in
+  let unroll_exp e = unroll_exp ty keys e in
   let has_target_type e = has_target_type ty e in
   let get_index k = get_index keys k in
   match e.e with
@@ -34,22 +34,22 @@ let rec unroll_in_exp
   | EFun f ->
     efun
       { f with
-        argty= None; resty= None; body= unroll_in_exp f.body }
+        argty= None; resty= None; body= unroll_exp f.body }
   | EApp (e1, e2) ->
-    eapp (unroll_in_exp e1) (unroll_in_exp e2)
+    eapp (unroll_exp e1) (unroll_exp e2)
   | EIf (e1, e2, e3) ->
-    eif (unroll_in_exp e1) (unroll_in_exp e2) (unroll_in_exp e3)
+    eif (unroll_exp e1) (unroll_exp e2) (unroll_exp e3)
   | ELet (x, e1, e2) ->
-    elet x (unroll_in_exp e1) (unroll_in_exp e2)
+    elet x (unroll_exp e1) (unroll_exp e2)
   | ETuple es ->
-    etuple (List.map unroll_in_exp es)
+    etuple (List.map unroll_exp es)
   | ESome e1 ->
-    esome (unroll_in_exp e1)
+    esome (unroll_exp e1)
   | EMatch (e1, bs) ->
     ematch
-      (unroll_in_exp e1)
-      (List.map (fun (p, e) -> (p, unroll_in_exp e)) bs)
-  | ETy (e1, _) -> unroll_in_exp e1
+      (unroll_exp e1)
+      (List.map (fun (p, e) -> (p, unroll_exp e)) bs)
+  | ETy (e1, _) -> unroll_exp e1
   | EOp (op, es) ->
     match op, es with
     | And, _
@@ -60,12 +60,12 @@ let rec unroll_in_exp
     | ULess _, _
     | ULeq _, _
     | UEq, _ ->
-      eop op (List.map unroll_in_exp es)
+      eop op (List.map unroll_exp es)
     | MCreate, [e] ->
-      etuple (BatList.make (List.length keys) (unroll_in_exp e))
+      etuple (BatList.make (List.length keys) (unroll_exp e))
     | MGet, [map; k] ->
       if not (has_target_type map) then
-        eop MGet [unroll_in_exp map; unroll_in_exp k]
+        eop MGet [unroll_exp map; unroll_exp k]
       else
         let index =
           match get_index k with
@@ -81,14 +81,14 @@ let rec unroll_in_exp
             BatList.make (List.length keys - index - 1) PWild
           )
         in
-        ematch (unroll_in_exp map) [(pattern, evar x)]
+        ematch (unroll_exp map) [(pattern, evar x)]
     | MSet, [map; k; setval] ->
       if not (has_target_type map) then
-        eop MSet [unroll_in_exp map; unroll_in_exp k; unroll_in_exp setval]
+        eop MSet [unroll_exp map; unroll_exp k; unroll_exp setval]
       else
         begin
           match get_index k with
-          | None -> unroll_in_exp map
+          | None -> unroll_exp map
           | Some index ->
             let freshvars =
               List.map (fun _ -> Var.fresh "UnrollingSetVar") keys
@@ -100,16 +100,16 @@ let rec unroll_in_exp
               List.mapi
                 (fun i var ->
                    if i <> index then evar var
-                   else unroll_in_exp setval)
+                   else unroll_exp setval)
                 freshvars
             in
-            ematch (unroll_in_exp map) [(pattern, etuple result)]
+            ematch (unroll_exp map) [(pattern, etuple result)]
         end
     | MMap, [f; map] ->
       if not (has_target_type map) then
-        eop MMap [unroll_in_exp f; unroll_in_exp map]
+        eop MMap [unroll_exp f; unroll_exp map]
       else
-        let f' = unroll_in_exp f in
+        let f' = unroll_exp f in
         let freshvars =
           List.map (fun _ -> Var.fresh "UnrollingMapVar") keys
         in
@@ -121,13 +121,13 @@ let rec unroll_in_exp
             (fun var -> eapp f' (evar var))
             freshvars
         in
-        ematch (unroll_in_exp map) [(pattern, etuple result)]
+        ematch (unroll_exp map) [(pattern, etuple result)]
     | MMapFilter, [p; f; map] ->
       if not (has_target_type map) then
-        eop MMapFilter [unroll_in_exp p; unroll_in_exp f; unroll_in_exp map]
+        eop MMapFilter [unroll_exp p; unroll_exp f; unroll_exp map]
       else
-        let f' = unroll_in_exp f in
-        let p' = unroll_in_exp p in
+        let f' = unroll_exp f in
+        let p' = unroll_exp p in
         let freshvars =
           List.map (fun _ -> Var.fresh "UnrollingMapFilterVar") keys
         in
@@ -140,21 +140,21 @@ let rec unroll_in_exp
         let result =
           List.map2 make_result keys freshvars
         in
-        ematch (unroll_in_exp map) [(pattern, etuple result)]
+        ematch (unroll_exp map) [(pattern, etuple result)]
     | MMerge, [f; map1; map2] ->
       if not ((has_target_type map1) && (has_target_type map2)) then
-        eop MMerge [unroll_in_exp f; unroll_in_exp map1; unroll_in_exp map2]
+        eop MMerge [unroll_exp f; unroll_exp map1; unroll_exp map2]
       else
-        let f' = unroll_in_exp f in
+        let f' = unroll_exp f in
         let freshvars1, freshvars2 =
           List.map (fun _ -> Var.fresh "UnrollingMMergeVar1") keys,
           List.map (fun _ -> Var.fresh "UnrollingMMergeVar2") keys
         in
         let pattern =
           PTuple([
-            PTuple (List.map (fun var -> PVar var) freshvars1);
-            PTuple (List.map (fun var -> PVar var) freshvars2)
-          ])
+              PTuple (List.map (fun var -> PVar var) freshvars1);
+              PTuple (List.map (fun var -> PVar var) freshvars2)
+            ])
         in
         let result =
           List.map2
@@ -162,19 +162,57 @@ let rec unroll_in_exp
             freshvars1 freshvars2
         in
         ematch
-          (etuple [unroll_in_exp map1; unroll_in_exp map2])
+          (etuple [unroll_exp map1; unroll_exp map2])
           [(pattern, etuple result)]
     | _ ->
       failwith "Failed to unroll map: Incorrect number of arguments to map operation"
 ;;
 
-let unroll_in_decl
+let unroll_type
+    (ty : ty)
+    (keys : exp list)
+    (ty2 : ty)
+  : ty
+  =
+  if not (equal_tys ty ty2) then ty2 else
+    match ty with
+    | TMap (_, key_ty) ->
+      TTuple (BatList.make (List.length keys) key_ty)
+    | _ ->
+      failwith "unroll_type: cannot unroll non-map type"
+;;
+
+let unroll_decl
     (ty : ty)
     (keys : exp list)
     (decl : declaration)
   : declaration
   =
-  failwith "AAS"
+  let unroll_exp = unroll_exp ty keys in
+  let unroll_type = unroll_type ty keys in
+  match decl with
+  | DLet (var, tyo, e) ->
+    let tyo' =
+      match tyo with
+      | Some t -> Some(unroll_type t)
+      | None -> None
+    in
+    DLet (var, tyo', unroll_exp e)
+  | DSymbolic (var, toe) ->
+    let toe' =
+      match toe with
+      | Ty t -> Ty(unroll_type t)
+      | Exp e -> Exp(unroll_exp e)
+    in
+    DSymbolic (var, toe')
+  | DATy t -> DATy (unroll_type t)
+  | DMerge e -> DMerge (unroll_exp e)
+  | DTrans e -> DTrans (unroll_exp e)
+  | DInit e -> DInit (unroll_exp e)
+  | DAssert e -> DAssert (unroll_exp e)
+  | DRequire e -> DRequire (unroll_exp e)
+  | DNodes _
+  | DEdges _ -> decl
 ;;
 
 let unroll_one_type
@@ -186,5 +224,5 @@ let unroll_one_type
   (* According to the docs, ExpSet.elements returns a sorted list.
      This is important because we need a consistent numbering for
      our keys *)
-  List.map (unroll_in_decl ty (ExpSet.elements keys)) decls
+  List.map (unroll_decl ty (ExpSet.elements keys)) decls
 ;;
