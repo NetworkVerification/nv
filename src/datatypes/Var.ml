61695d50fa1de2571f9f417524e8f17505bc3388
Devon Loehr <dloehr@princeton.edu>
2019-05-28 15:39:55 -0400

    Update Vars to use the ~ character as a delimiter in to_string, and add inverse function of to_string

--- src/datatypes/Var.ml
+++ src/datatypes/Var.ml
@@ -1,5 +1,8 @@
 type t = string * int [@@deriving show]
 
+(* Should be a character which the NV parser doesn't allow in identifiers *)
+let delim = "~"
+
 let counter = ref 0
 
 let next () =
@@ -18,7 +21,15 @@ let to_var (s, i) = (s, i)
 
 let from_var (s, i) = (s, i)
 
-let to_string (s, i) = s ^ string_of_int i
+let to_string (s, i) = s ^ delim ^ string_of_int i
+
+let of_var_string s =
+  try
+    let v, i = BatString.rsplit s ~by:"~" in
+    (v, int_of_string i)
+  with
+  | Not_found ->
+    failwith @@ Printf.sprintf "of_var_string: %s has wrong format (no '~')" s
 
 let equal (s1, i1) (s2, i2) = s1 = s2 && i1 = i2
 
