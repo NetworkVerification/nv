fd1fb249280c28f8156d13be2cd3aab80b42589c
Devon Loehr <dloehr@princeton.edu>
2019-06-12 14:08:38 -0400

    Move around some utility files

--- src/Console.mli
+++ src/Console.mli
@@ -1,3 +1,5 @@
+module T = ANSITerminal
+
 type info = {input: string array; linenums: (int * int) array}
 
 val error : string -> 'a
@@ -7,3 +9,9 @@ val warning : string -> unit
 val error_position : info -> Span.t -> string -> 'a
 
 val warning_position : info -> Span.t -> string -> unit
+
+val get_position : int -> info -> int * int
+
+val get_position_opt : int -> info -> (int * int) option
+
+val show_message : string -> T.color -> string -> unit
