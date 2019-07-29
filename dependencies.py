c9b3639ebe1de06dfa1f651a4a001a5713763e05
Devon Loehr <dloehr@princeton.edu>
2019-07-29 13:55:21 -0400

    Beef up dependency generator by adding more filters

--- dependencies.py
+++ dependencies.py
@@ -2,7 +2,50 @@ import sys
 import os
 import subprocess
 
-# Run with "python3 dependencies.py [srcdir] [--only-dune-libraries]"
+''' Run with
+"python3 dependencies.py [srcdir] [--only-dune-libraries] [--no-dune-libraries]
+[--no-externals] [--no-internal-modules]"
+
+The arguments are as follows:
+ * srcdir : The directory containing the files to run dependency analysis on
+ * --only-dune-libraries : Only show dependencies corresponding to dune libraries,
+                           i.e. the folders which contain a dune file
+ * --no-dune-libraries : Opposite of the above
+ * --no-externals : Don't show dependencies on external modules like Batteries
+ * --no-internal-modules : Resolve dependencies for modules which are declared
+                           inside a source file to that file. For example,
+                           a file which depends on ExpSet will be listed as
+                           depending on Collections instead.
+'''
+
+def is_external(s):
+    return s in \
+    ["Batteries", "BatList", "BatMap", "BatSet", "BatEnum", "ANSITerminal", \
+     "Hashtbl", "Lazy", "Printf", "Sys", "Array", "Pervasives", "Printexc", \
+     "Z", "Big_int_Z", "Weak", "Lru_cache", "Set", "Int64", "Unix", "BatUnix", \
+     "Thread", "BatPervasives", "BatString", "Unsigned", "PMap", "PSet", "List", \
+     "Buffer", "Graph", "Queue", "Str", "String", "Map", "Cudd", "BatOption",
+     "BatUref"]
+
+def is_dune_library(s):
+    return s.startswith("Nv_")
+
+# Map modules which are declared inside a file to that file; return all other
+# module names unchanges
+def map_to_src_file(s):
+    modulemap = {"EdgeMap" : "AdjGraph", \
+                 "ExpMap" : "Collections", \
+                 "ExpSet" : "Collections", \
+                 "ValueSet" : "Collections", \
+                 "ValueMap" : "Collections", \
+                 "TypeMap" : "Collections", \
+                 "ConstantSet" : "SmtUtils", \
+                 "IntMap" : "PrimitiveCollections", \
+                 "IntSet" : "PrimitiveCollections", \
+                 "StringMap" : "PrimitiveCollections", \
+                 "StringSet" : "PrimitiveCollections", \
+                 "StringSetSet" : "PrimitiveCollections"}
+    return modulemap.get(s, s)
 
 # Call ocamldep
 def getAllDeps(srcdir) :
@@ -16,7 +59,7 @@ def getAllDeps(srcdir) :
     return file_and_deps
 
 # Put all dependencies into a dictionary that mirrors the filesystem
-def parse_ocamldep_output(lines, allDeps, onlyDuneLibs):
+def parse_ocamldep_output(args, lines, allDeps):
     for line in lines:
         file = line[0][:-1]
         filepath = file.split("/")[1:]
@@ -28,8 +71,15 @@ def parse_ocamldep_output(lines, allDeps, onlyDuneLibs):
             deps[dir] = deps.get(dir, {})
             deps = deps[dir]
         fdeps = line[1:]
-        if onlyDuneLibs:
-            fdeps = [d for d in fdeps if d.startswith("Nv_")]
+        if args.get("no-externals", False):
+            fdeps = [d for d in fdeps if not is_external(d)]
+        if args.get("only-dune-libraries", False):
+            fdeps = [d for d in fdeps if is_dune_library(d)]
+        if args.get("no-dune-libraries", False):
+            fdeps = [d for d in fdeps if not is_dune_library(d)]
+        if args.get("no-internal-modules", False):
+            fdeps = list(set([map_to_src_file(d) for d in fdeps]))
+        fdeps.sort()
         deps[filename] = fdeps # Add dependencies for this file
 
 # Compute dependencies for each directory
@@ -65,9 +115,13 @@ def parse_args():
             args["srcdir"] = arg
         elif arg == "--only-dune-libraries":
             args["only-dune-libraries"] = True
+        elif arg == "--no-externals":
+            args["no-externals"] = True
+        elif arg == "--no-internal-modules":
+            args["no-internal-modules"] = True
+        elif arg == "--no-dune-libraries":
+            args["no-dune-libraries"] = True
     return args
-
-
 
 def main():
     args = parse_args()
@@ -77,7 +131,7 @@ def main():
     allDeps = {}
 
     ocamldep_output = getAllDeps(srcdir)
-    parse_ocamldep_output(ocamldep_output, allDeps, args["only-dune-libraries"])
+    parse_ocamldep_output(args, ocamldep_output, allDeps)
     compute_dirdeps(allDeps)
     print_deps(allDeps, srcdir, "")
 
