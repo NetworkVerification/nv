36d0c57799adcedc7df6d6593592f3140f88ad54
Devon Loehr <dloehr@princeton.edu>
2019-09-10 13:26:24 -0400

    Update tests and add a test that uses failures

--- runtests.py
+++ runtests.py
@@ -1,4 +1,4 @@
-import subprocess
+import subprocess, os
 
 testfiles = [
 # "fattree.nv",
@@ -27,14 +27,16 @@ testfiles = ["examples/" + f for f in testfiles]
 
 def make_testfiles():
     filenames = []
-    topology_files = ["fattree20", "fattree20-2", "fattree125", "fattree500"]#, "fattree1000"]
-    assertions = ["match x[0n] with | None -> false | Some _ -> true", "foldNodes (fun k v acc -> acc && match v with | None -> false | Some _ -> true) x true"]
+    topology_files = ["fattree20", "fattree20-2", "fattree125", "fattree500", "fattree1000"]
+    assertions = [\
+        "match x with | None -> false | Some x -> match x[0n] with | None -> false | Some _ -> true", \
+        "match x with | None -> false | Some x -> foldNodes (fun k v acc -> acc && match v with | None -> false | Some _ -> true) x true"]
     for top in topology_files:
         for i, asn in enumerate(assertions):
             filename = "testfiles/" + top + ("_single" if i==0 else "_allpairs") + ".nv"
             f = open(filename, "w")
             f.write("include \"../examples/generic-topologies/" + top + ".nv\"\n")
-            f.write("include \"../examples/generic-algebras/distance-allpairs.nv\"\n")
+            f.write("include \"../examples/generic-algebras/distance-allpairs-failures.nv\"\n")
             f.write("let assert n x = " + asn + "\n")
             f.close()
             filenames.append(filename)
@@ -47,9 +49,17 @@ testfiles = make_testfiles()
 
 for file in testfiles:
     print("Running test on "+file)
-    outfile1 = open("output/{}_slicing.txt".format(getFilename(file)), "w")
-    # outfile2 = open("output/{}_vanilla.txt".format(getFilename(file)), "w")
-    cmd1 = ["./nv", "-m", "-slicing", file]
-    # cmd2 = ["./nv", "-m", "-unbox", file]
-    subprocess.run(cmd1, stdout=outfile1, stderr=outfile1)
-    # subprocess.run(cmd2, stdout=outfile2, stderr=outfile2)
+    ocaml_env = os.environ.copy()
+    ocaml_env["OCAMLRUNPARAM"] = "s=8M,i=64M,o=150"
+    outfile = open("output/{}_parallel4.txt".format(getFilename(file)), "w")
+    cmd = ["time", "./nv", "-m", "-slicing", "-p", "4", file]
+    subprocess.run(cmd, stdout=outfile, stderr=outfile, env=ocaml_env)
+
+    outfile = open("output/{}_slicing.txt".format(getFilename(file)), "w")
+    cmd = ["time", "./nv", "-m", "-slicing", file]
+    subprocess.run(cmd, stdout=outfile, stderr=outfile, env=ocaml_env)
+
+    if not ("500" in file or "1000" in file): # Don't even try without slicing
+        outfile = open("output/{}_vanilla.txt".format(getFilename(file)), "w")
+        cmd2 = ["time", "./nv", "-m", "-unbox", file]
+        subprocess.run(cmd2, stdout=outfile, stderr=outfile, env=ocaml_env)
