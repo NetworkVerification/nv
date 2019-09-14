b4353bd952569a7e7f417901da0f51b72773fe47
Tim Alberdingk Thijm <tim.thijm@gmail.com>
2019-09-13 23:32:19 -0400

    Comment AdjGraph.min_cut code, remove edges_map

--- src/lib/datastructures/AdjGraph.ml
+++ src/lib/datastructures/AdjGraph.ml
@@ -61,11 +61,6 @@ let of_edges (l: (Vertex.t * Vertex.t) list) : t =
    results. We should look into that to make sure the differences are legitimate,
    then remove this and switch to ocamlgraph's min-cut. *)
 
-(* return an EdgeMap where each edge key has a map of edge neighbors *)
-let edges_map (g: t) (f: Edge.t -> 'a) : 'a EdgeMap.t =
-  fold_edges_e (fun e a -> EdgeMap.add e (f e) a) g EdgeMap.empty
-
-
 (* return a (bool, VertexMap) tuple indicating whether the node t was visited and what the path to it was? *)
 let bfs (g: t) (rg : int EdgeMap.t) (s: Vertex.t) (t: Vertex.t) : (bool * Vertex.t VertexMap.t) =
   let visited = VertexSet.singleton s in
@@ -93,23 +88,32 @@ let bfs (g: t) (rg : int EdgeMap.t) (s: Vertex.t) (t: Vertex.t) : (bool * Vertex
   let path, visited = loop path visited in
   (VertexSet.mem t visited, path)
 
-(* return the set of vertices reachable from s via DFS taking paths with positive flow? *)
+
+(* return the set o vertices reachable from s via DFS taking paths with positive flow? *)
 let dfs (g: t) (rg : int EdgeMap.t) (s: Vertex.t) : VertexSet.t =
   let rec loop u visited =
     let visited = VertexSet.add u visited in
     fold_succ
      (fun v accvs ->
+       (* if there is some _positive_ edge from u to a neighbour v,
+        * continue the loop at v, otherwise stop *)
         if ((EdgeMap.find (u,v) rg > 0) && (not (VertexSet.mem v accvs))) then
           loop v accvs
         else accvs) g u visited
   in
   loop s VertexSet.empty
 
+(* module Dfs = Traverse.Dfs(Persistent.Digraph.Concrete(Vertex)) *)
+(* module Bfs = Traverse.Bfs(Persistent.Digraph.Concrete(Vertex)) *)
+
 let min_cut g s t =
-  let rg = ref (edges_map g (fun _ -> 1)) in
+  (* rg is a map of edges to weights, initialized to 1 at every edge *)
+  let rg = ref (fold_edges_e (fun e a -> EdgeMap.add e 1 a) g EdgeMap.empty) in
+  (* modified Edmonds-Carp algorithm *)
   let rec loop reach path =
     if reach then
       begin
+        (* compute min flow capacity from s to t along the path *)
         let path_flow = ref max_int in
         let u = ref t in
         while (!u <> s) do
@@ -117,6 +121,7 @@ let min_cut g s t =
           u := VertexMap.find !u path;
           path_flow := min !path_flow (EdgeMap.find (!u, v) !rg)
         done;
+        (* update edge weights using the capacity *)
         u := t;
         while (!u <> s) do
           let v = !u in (*current node*)
@@ -124,13 +129,18 @@ let min_cut g s t =
           rg := EdgeMap.modify (!u, v) (fun n -> n - !path_flow) !rg;
           rg := EdgeMap.modify (v, !u) (fun n -> n + !path_flow) !rg
         done;
+        (* recompute reachability check *)
         let reach, path = bfs g !rg s t in
         loop reach path
       end
   in
+  (* reach represents the existence of a path from s to t;
+   * path is the shortest path from s to t
+   *)
   let reach, path = bfs g !rg s t in
   loop reach path;
 
+  (* return the s-set produced by min cut *)
   let visited = dfs g !rg s in
   let cut = EdgeMap.fold (fun (u,v) _ cutset ->
       if (VertexSet.mem u visited) && (not (VertexSet.mem v visited)) then
@@ -138,6 +148,7 @@ let min_cut g s t =
       else
         cutset) !rg EdgeSet.empty
   in
+  (* return the cut-set, the visited vertices (s-set) and the non-visited vertices (t-set) *)
   (cut, visited, VertexSet.diff (vertices g) visited)
 
 module DrawableGraph = struct
