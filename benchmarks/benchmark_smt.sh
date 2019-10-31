89f101b74b50416295fbe5f98ea6922f96571860
Nick Giannarakis <ng8@princeton.edu>
2019-10-31 16:02:19 -0400

    added shortest path fat trees to script

--- benchmarks/benchmark_smt.sh
+++ benchmarks/benchmark_smt.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
 NV="../_build/default/src/exe/main.exe"
-BENCHMARKS="SinglePrefix/FAT4/fat4pol.nv
+BENCHMARKS="SinglePrefix/FAT4/sp4.nv
+SinglePrefix/FAT4/fat4pol.nv
+SinglePrefix/FAT8/sp8.nv
 SinglePrefix/FAT8/fat8pol.nv
+SinglePrefix/FAT10/sp10.nv
 SinglePrefix/FAT10/fat10pol.nv"
 
 for file in $BENCHMARKS
 do
     time "$NV" -m "$file";
+    echo "\n";
 done
