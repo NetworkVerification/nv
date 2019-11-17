3707f36cf9e727f10b533ecc969669505903c0ed
Nick Giannarakis <ng8@princeton.edu>
2019-11-17 12:27:40 -0500

    fixed script

--- benchmarks/benchmark_simulator.sh
+++ benchmarks/benchmark_simulator.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
 NV="../_build/default/src/exe/main.exe"
-BENCHMARKS="AllPrefixes/FAT20/fat20PolSim.nv"
+BENCHMARKS="AllPrefixes/FAT20/fat20PolSim.nv
+AllPrefixes/FAT24/fat24PolSim.nv
+AllPrefixes/FAT28/fat28PolSim.nv
+AllPrefixes/FAT32/fat32PolSim.nv"
 
 for file in $BENCHMARKS
 do
-    time "$NV" -inline -compile "$file";
+    time "$NV" -compile "$file";
     echo "\n";
 done
