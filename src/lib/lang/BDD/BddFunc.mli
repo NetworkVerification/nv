c653b6efd989187333b4c44034c96d2c1e22a1a6
gian <nick.giannarakis@gmail.com>
2019-12-17 11:54:08 -0500

    working on lifting evaluation for mapIf

--- src/lib/lang/BDD/BddFunc.mli
+++ src/lib/lang/BDD/BddFunc.mli
@@ -4,9 +4,10 @@ open Syntax
 type t =
   | BBool of Bdd.vt
   | BInt of Bdd.vt array
-  | BTuple of t list
   | BOption of Bdd.vt * t
-
+  | Tuple of t list
+  | BMap of BddMap.t
+  | Value of Syntax.value
 
 val bdd_of_bool: bool -> Cudd.Man.v Cudd.Bdd.t
 
