3c281be1c35100c06a9db6c92f8a1a25d02c7578
Tim Alberdingk Thijm <tim.thijm@gmail.com>
2020-01-18 10:56:25 -0500

    Add prototype transformer impl for SRP remapping
    The prototype implementation just handles remapping node and edge
    patterns and values.
    It is currently untested and may cause all kinds of weird issues.
    One main case that is not properly handled at present is that, while we
    can remap nodes/edges that do have a remapping, some nodes/edges don't.
    These cases may need to be dropped from the graph entirely so that bugs
    don't occur (or maybe they won't do anything?).
    
    We also don't have a mapback transformer implemented yet.
    This may not be desirable since the SRP is being split, although it
    would help a user debug the splits.
    To do this well, we'd need to be able to disambiguate which partition a
    node falls into: should each new node also keep track of which SRP it's
    in, so we can unambiguously tell them apart?
    This is getting increasingly complicated.

--- src/lib/kirigami/SrpRemapping.ml
+++ src/lib/kirigami/SrpRemapping.ml
@@ -3,6 +3,7 @@
 open Batteries
 open Nv_datastructures.AdjGraph
 open Nv_lang.Syntax
+open Nv_transformations
 
 (** A type for keeping track of the input to base and output to base relationships. *)
 type interface = {
@@ -178,3 +179,42 @@ let partition_edges (nodes: Vertex.t list) (edges: Edge.t list) (partf: Vertex.t
   (* add 1 to num_srps to convert from max partition # to number of SRPS *)
   let divided_nodes = divide_vertices node_srp_map (num_srps + 1) in 
   divide_edges edges intf divided_nodes
+
+let ty_transformer _ ty = Some ty
+
+let pattern_transformer (part_srp: partitioned_srp) (_: Transformers.recursors) p t =
+  match (p, t) with
+  | (PNode n, TNode) -> begin let new_node = VertexMap.find_default None n part_srp.node_map in
+    Option.map (fun n -> PNode n) new_node end
+  | (PEdge (PNode n1, PNode n2), TEdge) -> begin
+    let new_edge = EdgeMap.find_default None (n1, n2) part_srp.edge_map in
+    Option.map (fun (n1, n2) -> PEdge (PNode n1, PNode n2)) new_edge
+    end
+  | _ -> None
+
+let value_transformer (part_srp: partitioned_srp) _ v =
+  let make_node n = avalue (vnode n, Some TNode, v.vspan) in
+  let make_edge e = avalue (vedge e, Some TEdge, v.vspan) in
+  match v.v with
+  | VNode n -> let new_node = VertexMap.find_default None n part_srp.node_map in
+    Option.map (fun n -> make_node n) new_node
+  | VEdge e  -> let new_edge = EdgeMap.find_default None e part_srp.edge_map in
+    Option.map (fun e -> make_edge e) new_edge
+  | _ -> None
+
+let exp_transformer _ _ = None
+
+let map_back_transformer (_part_srp: partitioned_srp) _ _ v _ =
+  (* TODO: not yet implemented *)
+  Some v
+
+(* not yet implemented *)
+let mask_transformer _ _ v _ = Some v
+
+let make_toplevel (part_srp: partitioned_srp) (toplevel_transformer : 'a Transformers.toplevel_transformer) =
+  toplevel_transformer ~name:"RemapSrp" ty_transformer (pattern_transformer part_srp) (value_transformer part_srp)
+    exp_transformer (map_back_transformer part_srp) mask_transformer
+
+let remap_declarations part_srp = make_toplevel part_srp Transformers.transform_declarations
+let remap_net part_srp = make_toplevel part_srp Transformers.transform_network
+let remap_srp part_srp = make_toplevel part_srp Transformers.transform_srp
