e9ee42fe8b04b77486ddaffc19072a5df6d15f48
Devon Loehr <dloehr@princeton.edu>
2020-02-28 17:21:34 -0500

    Start working on a python file for converting all our old nv files to the new format

---
+++ update_examples.py
@@ -0,0 +1,26 @@
+def update_file(file):
+    f = open(file, "r+")
+    has_assert = False
+    def process_line(line):
+        print(line)
+        if line.strip().startswith("let assert"):
+            has_assert = True
+            line = "let assert_node" + line.strip()[10:] + "\n"
+        if line.strip().startswith("let init"):
+            line = "let init_" + line.strip()[8:] + "\n"
+        if line.strip().startswith("let trans"):
+            line = "let trans_" + line.strip()[9:] + "\n"
+        if line.strip().startswith("let merge"):
+            line = "let merge_" + line.strip()[9:] + "\n"
+        return line
+    lines = [process_line(line) for line in f.readlines()]
+    lines2 = [ "\n", "let init = init_\n",
+    "let trans = trans_\n",
+    "let merge = merge_\n\n",
+    "let sol = solution {init = init_; trans = trans_; merge = merge_}\n\n"]
+    if has_assert:
+        lines2.append("assert foldNodes (fun k v acc -> acc && assert_node k v) sol true")
+    f = open(file, "w")
+    f.writelines(lines + lines2)
+
+update_file ("examples/set.nv")
