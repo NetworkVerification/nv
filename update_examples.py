4e3734631d9466cb79c572c5798b487719a80583
Devon Loehr <dloehr@princeton.edu>
2020-03-06 12:51:54 -0500

    Convert all old examples to the new nv format by adding solution and assert statements

--- update_examples.py
+++ update_examples.py
@@ -20,22 +20,16 @@ def update_file(file):
             line = "let assert_node" + line.strip()[10:] + "\n"
         if line.strip().startswith("let init "):
             has_init = True
-            line = "let init_" + line.strip()[8:] + "\n"
         if line.strip().startswith("let trans "):
             has_trans = True
-            line = "let trans_" + line.strip()[9:] + "\n"
         if line.strip().startswith("let merge "):
             has_merge = True
-            line = "let merge_" + line.strip()[9:] + "\n"
         return line
     lines = [process_line(line) for line in f.readlines()]
-    if has_init: lines.append("let init = init_\n")
-    if has_trans: lines.append("let trans = trans_\n")
-    if has_merge: lines.append("let merge = merge_\n")
     if has_init and has_trans and has_merge:
-        lines.append("let sol = solution {init = init_; trans = trans_; merge = merge_}\n\n")
+        lines.append("\nlet sol = solution {init = init; trans = trans; merge = merge}\n")
     if has_assert:
-        lines.append("assert foldNodes (fun k v acc -> acc && assert_node k v) sol true\n")
+        lines.append("\nassert foldNodes (fun k v acc -> acc && assert_node k v) sol true\n")
     f = open(file, "w")
     f.writelines(lines)
 
