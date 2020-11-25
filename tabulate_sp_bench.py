2348ff88413e127b6f457eded9f28ce92ca1eabc
Tim Alberdingk Thijm <tim.thijm@gmail.com>
2020-11-25 16:31:42 -0500

    Fix bug in tabulate, some minor housekeeping

--- tabulate_sp_bench.py
+++ tabulate_sp_bench.py
@@ -16,6 +16,7 @@ LIST_OPERATIONS = 1
 # produce separate keys to distinguish each operation's time taken
 DISTINCT_OPERATIONS = 2
 
+
 def mean_float_dict(dicts, multiop=LIST_OPERATIONS):
     """
     Average the results across the given list of dictionaries.
@@ -41,10 +42,11 @@ def mean_float_dict(dicts, multiop=LIST_OPERATIONS):
         # otherwise, just return a list of each time that operation was profiled
         elif multiop == DISTINCT_OPERATIONS:
             for (i, nv) in enumerate(newval):
-                averaged[key + "_" + str(i)] = nv
+                averaged[key + " " + str(i)] = nv
         else:
             raise NotImplemented()
     return averaged
+
 
 def join_result_dicts(parted, unparted):
     """
@@ -53,9 +55,10 @@ def join_result_dicts(parted, unparted):
     """
     joined = dict()
     for (key, val) in parted.items():
-        joined[key + " (parted)"] = val
+        joined[key + " (part)"] = val
     joined.update(unparted)
     return joined
+
 
 def parse_output(output):
     """
@@ -77,13 +80,16 @@ def parse_output(output):
         profile.setdefault(stat, list()).append(qua)
     return profile
 
+
 def run_command(com, time):
     """
     Run the given command and capture its output.
     If it doesn't finish within the given time, kill it.
     """
     try:
-        proc = subprocess.run(com, text=True, check=True, capture_output=True, timeout=time)
+        proc = subprocess.run(
+            com, text=True, check=True, capture_output=True, timeout=time
+        )
         return parse_output(proc.stdout)
     except subprocess.CalledProcessError as exn:
         print(exn.stderr)
@@ -91,6 +97,7 @@ def run_command(com, time):
     except subprocess.TimeoutExpired as exn:
         print(exn.stderr)
         return {}
+
 
 def run_benchmark(dirformat, nameformat, size, time, trials, multiop):
     """
@@ -103,26 +110,28 @@ def run_benchmark(dirformat, nameformat, size, time, trials, multiop):
         print("Did not find an 'nv' executable in the current working directory!")
         sys.exit(1)
     # run nv with verbose, SMT and partitioning flags
-    com = [nvpath, "-v", "-m", "-k"]
-    partf = os.path.join(benchdir, nameformat.format(size, "-vpart1"))
+    com = [nvpath, "-v", "-m"]
+    partf = os.path.join(benchdir, nameformat.format(size, "-part1"))
     unpartf = os.path.join(benchdir, nameformat.format(size, ""))
     runs = []
     for i in range(trials):
         print("Running trial " + str(i + 1) + " of " + str(trials))
-        partcom = run_command(com + [partf], time)
+        partcom = run_command(com + ["-k", partf], time)
         unpartcom = run_command(com + [unpartf], time)
         runs.append(join_result_dicts(partcom, unpartcom))
     mean = mean_float_dict(runs, multiop)
     mean["Benchmark"] = benchdir
     return mean
 
+
 def write_csv(results, path):
     """Write the results dictionaries to a CSV."""
-    with open(path, 'w') as csvf:
-        writer = csv.DictWriter(csvf, fieldnames=results[0].keys(), restval='error')
+    with open(path, "w") as csvf:
+        writer = csv.DictWriter(csvf, fieldnames=results[0].keys(), restval="error")
         writer.writeheader()
         for result in results:
             writer.writerow(result)
+
 
 if __name__ == "__main__":
     DIRECTORY = "benchmarks/SinglePrefix/FAT{}"
@@ -134,4 +143,4 @@ if __name__ == "__main__":
     for sz in SIZES:
         print("Running benchmark " + DIRECTORY.format(sz))
         RUNS.append(run_benchmark(DIRECTORY, "sp{}{}.nv", sz, TIMEOUT, TRIALS, MULTIOP))
-    write_csv(RUNS, "kirigami-results-v1.csv")
+    write_csv(RUNS, "kirigami-results-h1.csv")
