734443bdb97521de5bc2a4d8cda434c9babedb8d
Tim Alberdingk Thijm <tim.thijm@gmail.com>
2020-12-15 12:31:45 -0500

    Clean up TransformDecls
    Remove unused TransformDecls code

--- src/lib/kirigami/TransformDecls.ml
+++ src/lib/kirigami/TransformDecls.ml
@@ -6,12 +6,7 @@ open Syntax
 open Nv_interpreter
 open SrpRemapping
 
-let node_to_exp n = annot TNode (e_val (vnode n))
-let edge_to_exp e = annot TEdge (e_val (vedge e))
-let node_to_pat node = exp_to_pattern (node_to_exp node)
-let edge_to_pat edge = exp_to_pattern (edge_to_exp edge)
-
-let etrue = aexp (e_val (vbool true), Some TBool, Span.default)
+let ebool b = aexp (e_val (vbool b), Some TBool, Span.default)
 
 let rec remap_pattern parted_srp p =
   let { node_map; edge_map; _ } = parted_srp in
@@ -103,11 +98,11 @@ and remap_branches parted_srp bs =
   List.fold_right (fun (p, e) b -> addBranch p e b) (List.rev pat_exps) emptyBranch
 
 and remap_exp_op parted_srp op es =
-  print_endline (Printing.exp_to_string (eop op es));
+  (* print_endline (Printing.exp_to_string (eop op es)); *)
   let f = remap_exp parted_srp in
   let ty = (List.hd es).ety |> Option.get in
-  (* check if the operation is over nodes or edges *)
-  (* if so, if any nodes or edges are cut, the op simplifies to false *)
+  (* check if the operation is over nodes *)
+  (* if so, if any nodes are cut, the op simplifies to false *)
   match ty with
   | TNode ->
     (match op with
@@ -117,11 +112,11 @@ and remap_exp_op parted_srp op es =
          if (is_value n) then
            Option.map e_val (remap_value parted_srp (to_value n))
          else Some (f n)
-        in
-        let es1 = List.map remap_node_exp es in
-        if List.exists Option.is_none es1 then
-          annot TBool (e_val (vbool false))
-        else eop op (List.map Option.get es1)
+       in
+       let es1 = List.map remap_node_exp es in
+       if List.exists Option.is_none es1 then
+         ebool false
+       else eop op (List.map Option.get es1)
      | _ -> failwith (Printf.sprintf "unexpected operator %s over nodes" (show_op op)))
   | TEdge -> failwith "not implemented; edges should already be unboxed"
   | _ -> eop op (List.map f es)
@@ -134,216 +129,10 @@ let remap_solve parted_srp solve =
   { solve with init; trans; merge }
 ;;
 
-(* Create an annotated match statement *)
-let amatch v t b = annot t (ematch (aexp (evar v, Some t, Span.default)) b)
-
-(** Add match branches using the given map of old nodes to new nodes. *)
-let match_of_node_map
-    (m : Vertex.t option VertexMap.t)
-    (f : Vertex.t -> Vertex.t -> exp)
-    b
-  =
-  let add_node_branch old_node new_node branches =
-    match new_node with
-    | Some n -> addBranch (node_to_pat n) (f n old_node) branches
-    | None -> branches
-    (* if there is no new node, just map the old node to itself *)
-    (* addBranch (node_to_pat old_node) (node_to_exp old_node) branches *)
-  in
-  VertexMap.fold add_node_branch m b
-;;
-
-(** Add match branches using the given map of old edges to new edges. *)
-let match_of_edge_map (m : Edge.t option EdgeMap.t) b =
-  let add_edge_branch old_edge new_edge branches =
-    match new_edge with
-    | Some e -> addBranch (edge_to_pat e) (edge_to_exp old_edge) branches
-    | None -> branches
-  in
-  EdgeMap.fold add_edge_branch m b
-;;
-
-(* Apply and partially interpret the given transfer function t on the given edge e and attribute value x. *)
-let apply_trans ty e t x =
-  let u, v = e in
-  let trans_app = apps t [node_to_exp u; node_to_exp v] in
-  let trans_curried = annot (TArrow (ty, ty)) trans_app in
-  (* partially interpret the transfer function *)
-  InterpPartialFull.interp_partial (annot ty (eapp trans_curried x))
-;;
-
-(* Pass in the original init Syntax.exp and update it to perform
- * distinct actions for the inputs and outputs of the OpenAdjGraph.
- * The expression that is passed in should be a function which has
- * a single parameter of type tnode.
- *)
-let transform_init (init : exp) (trans : exp option) (merge : exp) ty parted_srp
-    : Syntax.exp
-  =
-  let interp = InterpPartialFull.interp_partial in
-  let ({ node_map; inputs; _ } : SrpRemapping.partitioned_srp) = parted_srp in
-  let node_var = Var.fresh "node" in
-  (* function we recursively call to build up the new base node init *)
-  let merge_input node node_exp input_exp =
-    let ({ var; edge; _ } : SrpRemapping.input_exp) = input_exp in
-    let input_exp = annot ty (evar var) in
-    (* perform the input transfer on the input exp *)
-    let trans_input_exp =
-      Option.apply (Option.map (apply_trans ty edge) trans) input_exp
-    in
-    (* perform the merge function, using the given node and its current value with the input variable *)
-    let curried_node =
-      interp (annot (TArrow (TArrow (ty, ty), ty)) (eapp merge (node_to_exp node)))
-    in
-    let curried_node_exp = annot (TArrow (ty, ty)) (eapp curried_node node_exp) in
-    interp (wrap node_exp (eapp curried_node_exp trans_input_exp))
-  in
-  (* we use both node names here since the inputs are organized acc. to new node name, while the old node name
-   * is needed to interpret the old function *)
-  let map_nodes new_node old_node =
-    let interp_old_node =
-      annot ty (InterpPartialFull.interp_partial_fun init [node_to_exp old_node])
-    in
-    match VertexMap.Exceptionless.find new_node inputs with
-    | Some input_nodes ->
-      List.fold_left
-        (fun e input -> merge_input old_node e input)
-        interp_old_node
-        input_nodes
-    | None -> interp_old_node
-  in
-  let branches = match_of_node_map node_map map_nodes emptyBranch in
-  (* the returned expression should be a function that takes a node as input with the following body:
-   * a match with node as the exp and output_branches as the branches *)
-  wrap
-    init
-    (efunc (funcFull node_var (Some TNode) (Some ty) (amatch node_var TNode branches)))
-;;
-
-(* Pass in the original trans Syntax.exp and update it to perform
- * distinct actions for the inputs and outputs of the OpenAdjGraph.
- * The expression that is passed in should be a function which has
- * two parameters of types tedge and attribute
- *)
-let transform_trans (e : exp) (attr : ty) (parted_srp : SrpRemapping.partitioned_srp)
-    : Syntax.exp
-  =
-  let ({ edge_map; _ } : SrpRemapping.partitioned_srp) = parted_srp in
-  (* new function argument *)
-  let edge_var = Var.fresh "edge" in
-  let x_var = Var.fresh "x" in
-  (* Simplify the old expression to an expression just over the second variable *)
-  let interp_trans edge =
-    InterpPartialFull.interp_partial (annot attr (apps e [edge; annot attr (evar x_var)]))
-  in
-  let edge_map_match = match_of_edge_map edge_map emptyBranch in
-  let branches = mapBranches (fun (pat, edge) -> pat, interp_trans edge) edge_map_match in
-  let body =
-    if isEmptyBranch branches then evar x_var else amatch edge_var TEdge branches
-  in
-  let x_lambda = efunc (funcFull x_var (Some attr) (Some attr) (annot attr body)) in
-  let lambda =
-    efunc (funcFull edge_var (Some TEdge) (Some (TArrow (attr, attr))) x_lambda)
-  in
-  wrap e lambda
-;;
-
-let transform_merge (e : exp) (ty : ty) (parted_srp : SrpRemapping.partitioned_srp) : exp =
-  let ({ node_map; _ } : SrpRemapping.partitioned_srp) = parted_srp in
-  (* the internal type after matching on the node *)
-  let inner_ty = TArrow (ty, TArrow (ty, ty)) in
-  let node_var = Var.fresh "node" in
-  let interp_merge node =
-    InterpPartialFull.interp_partial (annot inner_ty (eapp e node))
-  in
-  let branches =
-    match_of_node_map node_map (fun _ old -> interp_merge (node_to_exp old)) emptyBranch
-  in
-  wrap
-    e
-    (efunc
-       (funcFull node_var (Some TNode) (Some inner_ty) (amatch node_var TNode branches)))
-;;
-
-(* Check that the solution's value at a particular output vertex satisfies the predicate. *)
-let add_output_pred
-    (trans : exp option)
-    (attr : ty)
-    (sol : exp)
-    (n : Vertex.t)
-    (* (base_nodes : int) *)
-      (edge, pred)
-    acc
-  =
-  (* FIXME: figure out what this is after map unrolling *)
-  let sol_x = annot attr (eop MGet [sol; node_to_exp n]) in
-  (* let sol_x = annot attr (eop (TGet (base_nodes, n, n)) [sol]) in *)
-  match pred with
-  | Some p ->
-    InterpPartialFull.interp_partial
-      (annot
-         TBool
-         (eapp p (Option.apply (Option.map (apply_trans attr edge) trans) sol_x)))
-    :: acc
-  | None -> acc
-;;
-
-(* Check each output's solution in the sol variable. *)
-let outputs_assert
-    (trans : exp option)
-    (sol : exp)
-    (attr : ty)
-    (parted_srp : SrpRemapping.partitioned_srp)
-    : exp list
-  =
-  let ({ outputs; _ } : SrpRemapping.partitioned_srp) = parted_srp in
-  (* re-map every output to point to its corresponding predicate *)
-  (* let base_nodes = VertexMap.cardinal node_map in *)
-  let add_preds n outputs acc =
-    List.fold_left
-      (fun acc output -> add_output_pred trans attr sol n output acc)
-      acc
-      outputs
-  in
-  VertexMap.fold add_preds outputs []
-;;
-
-(** Collect all the conjuncts in the given expression. *)
-let rec collect_conjuncts l e =
-  (* print_endline (Printing.exp_to_string e); *)
-  match e.e with
-  | EOp (And, es) -> List.flatten (List.map (collect_conjuncts []) es) @ l
-  | _ -> e :: l
-  ;;
-
-(** Replace subexpressions in a nested series of EOp Ands according to the
- ** given rubric list: if the head of the rubric is true, keep the subexpression;
- ** if it is false, replace the subexpression with true. *)
-let rec prune_conjuncts rubric e =
-  (* FIXME: we don't want to burrow down super deeply; if a nested foldNodes is used,
-   * then we could end up with the wrong rubric length.
-   * additionally, internal projected elements could be used together as part of a
-   * single conjunct.
-   * a better approach may be  *)
-  print_endline (Nv_utils.OCamlUtils.list_to_string string_of_bool rubric);
-  print_endline (Printing.exp_to_string e);
-  match e.e with
-  | EOp (And, [e2; e3]) -> (match rubric with
-    | [] -> failwith "rubric shorter than number of conjuncts"
-    | h :: t -> let e2' = if h then e2 else etrue in
-      wrap e (eop And [e2'; prune_conjuncts t e3] ))
-  | EOp (And, _) -> failwith "and has wrong number of arguments"
-  (* this case should be the last one *)
-  | _ -> (match rubric with
-    | [] -> failwith "rubric shorter than number of conjuncts"
-    | [b] -> if b then e else etrue
-    | _ -> failwith "rubric longer than number of conjuncts")
-  ;;
-
-(* the and statement is nested as follows:
- * And (And (And x0 x1) x2) x3
- * so we need to recurse in until we have dropped
- * the right number of nodes?
+(** Remap an and expression by dropping conjuncts that refer to cut nodes.
+ ** The and statement is nested as follows:
+ ** And (And (And x0 x1) x2) x3
+ ** so we need to recurse in until we have dropped the right number of nodes.
  **)
 let rec remap_conjuncts nodes e =
   (* print_endline (Printing.exp_to_string e); *)
@@ -374,36 +163,13 @@ let rec remap_conjuncts nodes e =
  **)
 let transform_assert (e : exp) (parted_srp : SrpRemapping.partitioned_srp) : exp =
   let { nodes; _ } = parted_srp in
-  (* create a list of bool of length n * k; zip it against the conjuncts and replace any
-   * for which the corresponding bool is false with the statement "true" *)
-  (* FIXME: can't just drop the ones that refer to old nodes, as the assignment order
-   * has ALSO changed in the solutions; need to use the remap information as given.
-   * for instance, in the second partition of the network 0 = 1
-   * where 1 is remapped to 0, we want to drop the case of 0 and also change references
-   * to the solution for node 1 to references to the solution for node 0 *)
-  (* let rubric = VertexMap.fold (fun _ v l -> l @ (match v with
-   *     | Some _ -> List.make width true
-   *     | None -> List.make width false)) node_map [] in *)
-  (* TODO: drop expressions or simplify them to true if they reference nodes we don't have access to *)
   let e = (match e.e with
    | EMatch _ ->
-     (* TODO: if there is only one branch, use interp to simplify;
+     (* if there is only one branch, use interp to simplify;
       * we should be left with an EOp statement which we can prune *)
-     (* NOTE: this will also lead to a simplification of the "true" at the start of the
-      * statement, so our width should not be off by 1 *)
      let e1 = InterpPartialFull.interp_partial e in
      (match e1.e with
-     | EOp (And, _) ->
-       remap_conjuncts nodes e1
-       (* prune_conjuncts rubric e1 *)
-       (* let conjs = collect_conjuncts [] e1 in
-        * if (List.length conjs) = width then
-        *   print_endline "got all conjuncts"
-        * else
-        *   print_endline
-        *     ("found and length different from what I expected: expected "
-        *      ^ (string_of_int width) ^ ", got "
-        *      ^ (string_of_int (List.length conjs))) *)
+     | EOp (And, _) -> remap_conjuncts nodes e1
      | _ -> print_endline ("not an and: " ^ Printing.exp_to_string e1); e)
    | _ -> e) in
   remap_exp parted_srp e
@@ -413,32 +179,13 @@ let transform_assert (e : exp) (parted_srp : SrpRemapping.partitioned_srp) : exp
  *  from the interface expression.
 *)
 let interp_interface intfe e : exp option =
-  (* print_endline ("interface exp: " ^ Printing.exp_to_string intfe); *)
   let u, v = e in
   let node_value n = avalue (vnode n, Some Typing.node_ty, Span.default) in
   let edge = [node_value u; node_value v] in
-  let intf_app =
-    InterpPartial.interp_partial_fun
-      intfe
-      edge
-      (* [avalue (vedge e, Some Typing.edge_ty, Span.default)] *)
-  in
-  (* if intf_app is not an option, or if the value it contains is not a function,
-   * fail *)
+  let intf_app = InterpPartial.interp_partial_fun intfe edge in
   match intf_app.e with
   | EFun _ -> Some intf_app
   | EVal { v = VClosure _; _ } -> Some intf_app
-  (* | ESome exp -> Some exp
-   * | EVal { v = VOption o; _ } ->
-   *   begin
-   *     match o with
-   *     | Some { v = VClosure (_env, func); _ } -> Some (efunc func)
-   *     | Some _ -> failwith "expected a closure, got something else instead!"
-   *     (\* infer case *\)
-   *     | None -> None
-   *   end
-   * (\* ETuple case handles an unboxed option *\)
-   * | ETuple [{ e = EVal { v = VBool b; _ }; _ }; o] -> if b then Some o else None *)
   | _ ->
     failwith
       ("expected intf value to be a function but got " ^ Printing.exp_to_string intf_app)
@@ -461,95 +208,13 @@ let update_preds interface partitioned_srp =
   }
 ;;
 
-let transform_var_names var_names aty partitioned_srp =
-  let { nodes; _ } = partitioned_srp in
-  let new_width = nodes * match aty with
-    | TTuple ts -> List.length ts
-    | _ -> 1
-  in
-  match var_names.e with
-  | EVar _ -> var_names
-  | ETuple es ->
-    let ty = var_names.ety |> Option.get in
-    let ty = match ty with
-      | TTuple ts -> TTuple (List.take new_width ts)
-      | _ -> failwith "unexpected type"
-    in
-    annot ty (etuple (List.take new_width es))
-  | _ -> failwith "unexpected var_names exp"
-;;
-
-let collect_requires_and_symbolics ty partition
-    : (exp, int) Map.t * (var * ty_or_exp) list
-  =
-  let symb_to_var (v, toe) =
-    let ty = get_ty_from_tyexp toe in
-    annot ty (evar v)
-  in
-  let add_requires _ input_exps (m, l) =
-    List.fold_left
-      (fun (m, l) { var; rank; pred; _ } ->
-         (let symbs = Nv_transformations.TupleFlatten.proj_symbolic (var, Ty ty) in
-           (match pred with
-            | Some p ->
-              let args = List.map symb_to_var symbs in
-              Map.add
-                (InterpPartialFull.interp_partial
-                  (annot TBool (apps p args)))
-                rank
-                m
-            | None -> m)
-        , symbs @ l ))
-      (m, l)
-      input_exps
-  in
-  VertexMap.fold add_requires partition.inputs (Map.empty, [])
-;;
-
-let get_ranked_hypotheses ty partition : (exp, int) Map.t =
-  let symb_to_var (v, toe) =
-    let ty = get_ty_from_tyexp toe in
-    annot ty (evar v)
-  in
-  let add_requires _ input_exps m =
-    List.fold_left
-      (fun m { var; rank; pred; _ } ->
-         (* FIXME: need to make sure we're referring to the correct variables,
-          * which should already be floating around somewhere *)
-         let symbs = Nv_transformations.TupleFlatten.proj_symbolic (var, Ty ty) in
-           (match pred with
-            | Some p ->
-              let args = List.map symb_to_var symbs in
-              Map.add
-                (InterpPartialFull.interp_partial
-                  (annot TBool (apps p args)))
-                rank
-                m
-            | None -> m))
-      m
-      input_exps
-  in
-  VertexMap.fold add_requires partition.inputs Map.empty
-;;
-
-
 (* Transform the given solve and return it along with a new expression to assert
  * and new expressions to require. *)
 let transform_solve solve (partition : partitioned_srp)
     : partitioned_srp * solve
   =
-  let ({ init; trans; merge; interface; _ } : solve) = solve in
-  let partition' = update_preds interface partition in
-  let init = remap_exp partition' init in
-  let trans = remap_exp partition' trans in
-  let merge = remap_exp partition' merge in
-  (* collect require and symbolic information *)
-  partition',
-  { solve with
-      init
-    ; trans
-    ; merge
-    ; (* should this be erased, or kept as reference? *)
-      interface = None
-  }
+  let partition' = update_preds solve.interface partition in
+  let solve' = remap_solve partition' solve in
+  (* erase interface information now that it's in the partition *)
+  partition', { solve' with interface = None }
 ;;
