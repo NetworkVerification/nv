f22a59db80f6bb31386b5f4f5d10739cb2128757
Tim Alberdingk Thijm <tim.thijm@gmail.com>
2021-05-11 17:43:10 -0400

    Add map_exp functions to Visitors
    Add similar functions to iter_exp to Visitors for mapping expressions.
    These behave similarly to the transformers, but are only written for
    expressions and have a slightly different type signature from
    transformers, which uses ('a -> option 'a) functions to transform
    multiple syntactic types.

--- src/lib/lang/Visitors.ml
+++ src/lib/lang/Visitors.ml
@@ -1,6 +1,27 @@
 open Syntax
 open Batteries
 
+let rec map_exp f (e : exp) =
+  let e' = f e in
+  let e' =
+    match e'.e with
+    | EVar _ | EVal _ -> e'
+    | EOp (op, es) -> eop op (BatList.map (map_exp f) es)
+    | EFun fn -> efun { fn with body = map_exp f fn.body }
+    | EApp (e1, e2) -> eapp (map_exp f e1) (map_exp f e2)
+    | EIf (e1, e2, e3) -> eif (map_exp f e1) (map_exp f e2) (map_exp f e3)
+    | ELet (v, e1, e2) -> elet v (map_exp f e1) (map_exp f e2)
+    | ETuple es -> etuple (BatList.map (map_exp f) es)
+    | ESome e -> esome (map_exp f e)
+    | EMatch (e, bs) ->
+      ematch (map_exp f e) (mapBranches (fun (p, e) -> p, map_exp f e) bs)
+    | ETy (e, t) -> ety (map_exp f e) t
+    | ERecord map -> erecord (Collections.StringMap.map (fun e -> map_exp f e) map)
+    | EProject (e, s) -> eproject (map_exp f e) s
+  in
+  aexp (e', e.ety, e.espan)
+;;
+
 let rec iter_exp f (e : exp) =
   f e;
   match e.e with
@@ -27,6 +48,25 @@ let rec iter_exp f (e : exp) =
   | EProject (e, _) -> iter_exp f e
 ;;
 
+let map_exp_decl f d =
+  match d with
+  | DLet (v, oty, e) -> DLet (v, oty, map_exp (f d) e)
+  | DAssert e -> DAssert (map_exp (f d) e)
+  | DPartition e -> DPartition (map_exp (f d) e)
+  | DRequire e -> DRequire (map_exp (f d) e)
+  | DSymbolic (v, Exp e) -> DSymbolic (v, Exp (map_exp (f d) e))
+  | DSolve { aty; var_names; init; trans; merge; part } ->
+    DSolve
+      { aty
+      ; var_names = map_exp (f d) var_names
+      ; init = map_exp (f d) init
+      ; trans = map_exp (f d) trans
+      ; merge = map_exp (f d) merge
+      ; part = Option.map (map_part (map_exp (f d))) part
+      }
+  | DNodes _ | DEdges _ | DSymbolic _ | DUserTy _ -> d
+;;
+
 let iter_exp_decl f d =
   match d with
   | DLet (_, _, e)
@@ -44,4 +84,5 @@ let iter_exp_decl f d =
   | DNodes _ | DEdges _ | DSymbolic _ | DUserTy _ -> ()
 ;;
 
+let rec map_exp_decls f ds = BatList.map (map_exp_decl f) ds
 let rec iter_exp_decls f ds = BatList.iter (iter_exp_decl f) ds
--- src/lib/lang/Visitors.mli
+++ src/lib/lang/Visitors.mli
@@ -1,5 +1,8 @@
 open Syntax
 
+val map_exp : (exp -> exp) -> exp -> exp
+val map_exp_decl : (declaration -> exp -> exp) -> declaration -> declaration
+val map_exp_decls : (declaration -> exp -> exp) -> declarations -> declarations
 val iter_exp : (exp -> unit) -> exp -> unit
 val iter_exp_decl : (declaration -> exp -> unit) -> declaration -> unit
 val iter_exp_decls : (declaration -> exp -> unit) -> declarations -> unit
